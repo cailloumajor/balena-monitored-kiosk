036a3c92e45626083833f66ef974f949aa5691d0
Arnaud Rocher <arnaud.roche3@gmail.com>
2021-05-04 21:34:10 +0000

    Add tests for browser endpoint

--- browser/tests/app.spec.ts
+++ browser/tests/app.spec.ts
@@ -10,13 +10,48 @@
 */
 import express from "express"
 import request from "supertest"
+import { mocked } from "ts-jest/utils"
 
 import { createApp, configureTerminus } from "../src/app"
+import { launchInstance } from "../src/browser"
+
+jest.mock("../src/browser")
+const mockedLaunchInstance = mocked(launchInstance)
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
 test("health check endpoint", () => {
   const app = express()
   const server = configureTerminus(app)
   return request(server).get("/health").expect(200).expect({ status: "ok" })
+})
+
+describe("browser endpoint", () => {
+  test("fails if url is missing in request", () => {
+    return request(createApp()).post("/browser").send("").expect(400)
+  })
+
+  test("fails if launchInstance fails", () => {
+    mockedLaunchInstance.mockRejectedValue(new Error("testing error"))
+    return request(createApp())
+      .post("/browser")
+      .send("url=test_url")
+      .expect(500)
+  })
+
+  test("succeeds", () => {
+    mockedLaunchInstance.mockResolvedValue(1234)
+    return request(createApp())
+      .post("/browser")
+      .send("url=testurl")
+      .expect("Content-Type", /json/)
+      .expect(200, { devPort: 1234 })
+      .then(() => {
+        expect(mockedLaunchInstance).toHaveBeenCalledWith("testurl")
+      })
+  })
 })
 
 test("favicon request", () =>
